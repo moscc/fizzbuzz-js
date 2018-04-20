deac27847ad3daba2e379b1db715dbbaf13b1ca5
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-04-20 16:22:32 +0200

    Finished FizzBuzz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,6 +1,17 @@
 const FizzBuzz = {
   eval(number) {
-    // Write your code here
+
+    if (number % 15 == 0)
+        return 'FizzBuzz'
+
+    else if (number % 3 == 0)
+      return 'Fizz'
+
+    else if (number % 5 == 0)
+      return 'Buzz'
+
+    else
+      return (number)
   }
 };
 
